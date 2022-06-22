cce820ded716c378bb3a6a85f15cb21262ac661f
j2cey <jud10parfait@gmail.com>
2022-06-22 13:27:05 +0000

    delete nok, envoi mail markdown table

--- resources/views/emails/clientesims/profileesim.blade.php
+++ resources/views/emails/clientesims/profileesim.blade.php
@@ -8,7 +8,8 @@ Infos e-sim
 @endcomponent
 
 @component('mail::table')
-| -------------           |:-------------|
+| <!-- -->                | <!-- -->     |
+| -------------           |:-------------                                                              |
 | Nom/Raison Sociale      | <strong>{{ $client->nom_raison_sociale  }} {{ $client->prenom  }}</strong> |
 | Numero Telephone        | <strong>{{ $client->numero_telephone  }}</strong> |
 | Code PIN                | <strong>{{ $client->esim->pin  }}</strong> |
@@ -16,7 +17,7 @@ Infos e-sim
 @endcomponent
 
 @component('mail::panel')
-scannez le QR code pour telecharger votre profile E-SIM
+scannez ce QR code pour telecharger votre profile E-SIM
 @endcomponent
 
 <p>
