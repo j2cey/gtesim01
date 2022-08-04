38d1077d1d0020e04a8d4e55f26b864bd7544b0b
j2cey <jud10parfait@gmail.com>
2022-08-04 13:59:42 +0000

    before add employe to user

--- resources/js/app.js
+++ resources/js/app.js
@@ -47,7 +47,7 @@ import moment from "moment";
 
 Vue.filter("formatDate", function (value) {
     if (value) {
-        return moment(String(value)).format("DD/MM/YY HH:mm");
+        return moment(String(value)).format("DD/MM/YY HH:mm:ss");
     }
 });
 
